ab391f276b34f5767c44a98a8587c014b29953ef
Ba-Ski <color.ui>
2015-10-17 16:06:36 +0300

    first commit

---
+++ export.sql
@@ -0,0 +1,16 @@
+--------------------------------------------------------
+--  File created - Saturday-October-17-2015   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table PEOPLE
+--------------------------------------------------------
+
+  CREATE TABLE "BASKI"."PEOPLE" 
+   (	"NAME" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into BASKI.PEOPLE
+SET DEFINE OFF;
